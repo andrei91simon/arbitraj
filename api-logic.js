499812f012253cdce628d005b2610e1c3b647d2d
ovisimon <ovisimon@gmail.com>
2017-11-01 20:59:20 -0400

    Am aduagat si api de la bitfinex

--- api-logic.js
+++ api-logic.js
@@ -1,40 +1,40 @@
 $(document).ready(function(){
-
+/* var bitUrl = 'https://bittrex.com/api/v1.1/public/getmarketsummaries';
     //Api pentru Bittrex
-      /*  $.ajax({
+        $.ajax({
+        url: bitUrl,
         type: 'GET',
-        url: 'aici vine url',
         success: function(coins) {
-            $('#Omg-Bittrex').html(coins.omg_eth.buy + ' - ' + coins.omg_eth.sell);
-            $('#Req-Bittrex').html(coins.req_eth.buy + ' - ' + coins.req_eth.sell);
+            $('#Omg-Bittrex').html(coins.result[0].High);
+            $('#Req-Bittrex').html();
             $('#Mtl-Bittrex').html();
         },
         error: function() {
             alert('FAILURE!');
         }
-    });
-*/
+    }); */
         
     //Api pentru Binance
-   /* $.ajax({
+    $.ajax({
         type: 'GET',
-        url: 'aici vine url',
+        url: 'https://cors-anywhere.herokuapp.com/https://www.binance.com/api/v1/ticker/allPrices',
         success: function(coins) {
-            $('#Omg-Binance').html(coins.omg_eth.buy + ' - ' + coins.omg_eth.sell);
-            $('#Req-Binance').html(coins.req_eth.buy + ' - ' + coins.req_eth.sell);
-            $('#Mtl-Binance').html(coins.mtl_eth.buy + ' - ' + coins.mtl_eth.sell);
+            var myCo = JSON.parse(coins);
+            $('#Omg-Binance').html(myCo[31].price);
+            $('#Req-Binance').html(myCo[87].price);
+            $('#Mtl-Binance').html(myCo[60].price);
         },
         error: function() {
             alert('FAILURE!');
         }
-    });
-*/
+    }); 
         
     //Api pentru Liqui
     $.ajax({
         type: 'GET',
-        url: 'https://api.liqui.io/api/3/ticker/omg_eth-req_eth',
+        url: 'https://cors-anywhere.herokuapp.com/https://api.liqui.io/api/3/ticker/omg_eth-req_eth',
         success: function(coins) {
+            console.log(coins);
             $('#Omg-Liqui').html(coins.omg_eth.buy + ' - ' + coins.omg_eth.sell);
             $('#Req-Liqui').html(coins.req_eth.buy + ' - ' + coins.req_eth.sell);
             // nu exista api de mtl
@@ -42,7 +42,7 @@ $(document).ready(function(){
         error: function() {
             alert('FAILURE!');
         }
-    });
+});  
     
     //Api pentru Bitfinex
   /*  $.ajax({
@@ -59,3 +59,35 @@ $(document).ready(function(){
     });
 */
 })
+
+        //Api pentru Liqui
+var LiquiApi = function(){
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.liqui.io/api/3/ticker/omg_eth-req_eth',
+        success: function(coins) {
+            console.log(coins);
+            $('#Omg-Liqui').html(coins.omg_eth.buy + ' - ' + coins.omg_eth.sell);
+            $('#Req-Liqui').html(coins.req_eth.buy + ' - ' + coins.req_eth.sell);
+            // nu exista api de mtl
+        },
+        error: function() {
+            alert('FAILURE!');
+        }
+    }); 
+}
+        //Api pentru Binance
+/* var BinanceApi = function(){
+   $.ajax({
+        type: 'GET',
+        url: 'https://www.binance.com/api/v1/ticker/allPrices',
+        success: function(coins) {
+            var myCo = JSON.parse(coins);
+            $('#Omg-Binance').html(myCo[31].price);
+            $('#Req-Binance').html(myCo[87].price);
+            $('#Mtl-Binance').html(myCo[60].price);
+        },
+        error: function() {
+            alert('FAILURE!');
+        }
+    }); */
